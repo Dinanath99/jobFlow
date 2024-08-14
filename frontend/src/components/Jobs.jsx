9605153df2d6d52f62882d869d9061d09aa152c7
Dinanath <dkmdkm26@gmail.com>
2024-08-14 22:42:08 +0545

    job component added

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from "./shared/Navbar";
+import FilterCard from "./FilterCard";
+import Job from "./Job";
 
 const Jobs = () => {
-  return <div>Jobs</div>;
+  return (
+    <div>
+      <Navbar />
+      {/* filter card */}
+      <FilterCard />
+      {/* job card */}
+      <Job />
+    </div>
+  );
 };
 
 export default Jobs;
