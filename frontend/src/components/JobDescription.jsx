8d9f81e324286b0a97008186915305ed61acce1a
Dinanath <dkmdkm26@gmail.com>
2024-08-20 23:35:52 +0545

    job description added

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,7 +1,37 @@
+import { Badge } from "./ui/badge";
 import React from "react";
+import { Button } from "./ui/button";
 
-const Description = () => {
-  return <div>Description</div>;
+const JobDescription = () => {
+  const isApplied = true;
+  return (
+    <div className="max-w-7xl mx-auto my-10">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="font-bold text-xl">Title</h1>
+          <div className="flex items-center gap-2 mt-4">
+            <Badge className="text-blue-700 font-bold bg-slate-200 hover:bg-slate-400">
+              12 positions
+            </Badge>
+            <Badge className="text-[#F83002] font-bold bg-slate-200 hover:bg-slate-400">
+              Part Time
+            </Badge>
+            <Badge className="text-[#7209b7] font-bold bg-slate-200 hover:bg-slate-400">
+              16k
+            </Badge>
+          </div>
+        </div>
+        <Button
+          disabled={isApplied}
+          className={`rounded-lg ${
+            isApplied ? `bg-gray-600 cursor-not-allowed` : `bg-[#7209b7]`
+          }`}
+        >
+          {isApplied ? `Applied` : `Apply Now`}
+        </Button>
+      </div>
+    </div>
+  );
 };
 
-export default Description;
+export default JobDescription;
